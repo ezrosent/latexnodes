a7149619f88f48e42c0ab5b9587859d294b95db1
Eli Rosenthal <eli_rosenthal@brown.edu>
2014-07-02 23:35:07 -0700

    cleanup

--- LatexNodes.hs
+++ LatexNodes.hs
@@ -4,7 +4,6 @@ module LatexNodes where
 import Text.ParserCombinators.Parsec
 import Control.Applicative hiding ((<|>), optional, many)
 import Text.Printf (printf)
--- TODO: get nesting to work, then tables
 
 type Bullet = (Int,String)
 
@@ -33,84 +32,59 @@ parseLText = manyTill lTtoken (eof *> pure [])
               <|> try  (DMath  <$> (dbraces $ many1 (noneOf "#")))
               <|>       Math   <$> (braces  $ many1 (noneOf "#"))
         dbraces s = string "##" *> s <* string "##"
-        braces  = between (char '#') (char '#')
+        braces    = between (char '#') (char '#')
 
--- standard test thing for ghci
-ptest :: Show a => Parser a -> String -> IO ()
-ptest a s = print $ parse a "" s
-
+--- end parsing section
 stringOfLText :: LText -> String
 stringOfLText = \case Math s   -> printf "\\( %s \\)" s
                       DMath s  -> printf "\\[ %s \\]" s
                       Normal s -> s
-data BullTree = BT [[LText]] (Maybe BullTree)
--- a list of these things is a list of bullets, the next tree down, and
--- the continuation of the previous tree that was being build up
 
-
--- must convert to some tree datastructure.
-  -- Algorithm: combine all adjascent ones of same depth
-  -- when getting to n+1, return combine of all next level
-  -- when getting to n-1, return current
-  -- otherwise return error
-  -- currently this doesn't pass the extra bits back up, which it needs
-  -- to
-
-
-ebt :: [(Int,[LText])] -> BullTree
-ebt [] = BT [] Nothing
-ebt lst@((n,_):_) = let (yes,no) = split ((== n).fst) lst in
-                          BT (snd . combine $ yes) (disp no)
-  where split f ls = (takeWhile f ls,dropWhile f ls)
-        combine = (n,).(map snd)
-        disp = \case []              -> Nothing
-                     inls@((n',_):_) -> if | n' < n      -> Nothing
-                                           | n' == (n+1) -> Just $ ebt inls
-                                           | n' == n     -> error "programmer error"
-                                           | otherwise   -> error "jumpted too far ahead"
-
+-- decalre header-specific variables based on title and author
+-- TODO: don't hardcode author/title variables
+varDecs :: Doc -> String
+varDecs (Doc title author _) = printf "\\newcommand{\\mytitle}{%s}\
+  \ \n\\newcommand{\\myauthor}{%s}" title author
+-- emit laTex from a document
+-- TODO: have it return either rather than ad-hoc error throwing
 emitDoc :: Doc -> String
 emitDoc (Doc title author bullets) = printf "%s\n%s\n\\begin{document}\
   \ \n \\maketitle\n%s\n\\end{document}" title' author'  bullets'
   where title'   = (printf "\\title{%s}" title)   :: String
         author'  = (printf "\\author{%s}" author) :: String
-        {-bullets' = concatMap (emitLT  . lTextofBullet) bullets-}
         bullets' = emitBullet $ map lTextofBullet bullets
-        emitLT :: (Int,[LText]) -> String
-        emitLT (n,lts)   = printf "\n%s\n%s\n%s\n" (flatten' "\\begin{itemize}") lts' (flatten' "\\end{itemize}")
-          where lts'     = concatMap stringOfLText lts
-                flatten' = (concatMap id) . (replicate n)
+        emitBullet = emitBT True . conv
         lTextofBullet :: Bullet -> (Int,[LText])
         lTextofBullet (n,tex) = (n,rightOf $ parse parseLText "" tex)
         rightOf = \case Right a  -> Normal "\\item " : a
                         Left err -> error  $ "some sort of parse error happened" ++ show err
+
+-- intermediate data structure to hold bullets
+-- it has [list of bullets], optional children, optional continuation
+-- of bullets at same level after children, so It's a binary tree
 data  BT a = B [a] (Maybe (BT a)) (Maybe (BT a))
   deriving (Eq,Show)
 
-test :: [(Int,String)]
-test = [(1, "Hello"), (2, "There"), (3, "Sir"), (2, "How"), (1, "Dy"), (2,"Wat"),
-        (2, "A"), (3, "Find"), (3, "Down"), (1,"Here")]
-
+-- convert an assoc-list into a BT,
+-- the assoc-list has the semantics of 'Indentation Levels'
 conv :: Eq a => [(Int,a)] -> BT a
 conv = \case
   []           -> B [] Nothing Nothing
   xs@((n,_):_) -> B (map snd curr)
-                    (if (child == []) || (fhd child) < n then Nothing
+                    (if (child == []) || (fst $ head child) < n then Nothing
                              else Just $ conv child)
                     (if (next == []) then Nothing else Just $ conv next)
-    where fhd = fst . head
-          (curr,child,next) = split3 ((== n).fst) xs
+    where (curr,child,next) = split3 ((== n).fst) xs
           split3 f ls = (spl takeWhile,takeWhile (not.f) mid,end)
             where spl g = g f ls
                   mid = spl dropWhile
                   end = dropWhile (not.f) mid
--- not quite right with the begins and ends
+
+-- convert BT [LText] to well-itemized Latex;
+-- Boolean flag indicates whether or not to begin with a \begin{itemize}
 emitBT :: Bool ->  BT [LText] -> String
-emitBT b (B ls next cont)  = (if b then "\\begin{itemize}\n" else "")++
-                          bullets ++  children  ++ continue
+emitBT b (B ls next cont)  = printf "\
+\%s%s%s%s"   (if b then "\\begin{itemize}\n" else "") bullets children continue
   where bullets = concatMap id $ map (concatMap $ (++ "\n") .  stringOfLText) ls
         children = case next of {Nothing -> ""; Just s -> emitBT True s}
         continue = case cont of {Nothing -> "\\end{itemize}\n"; Just s -> emitBT False s}
-
-emitBullet :: [(Int,[LText])] -> String
-emitBullet = emitBT True . conv
--- Main.hs
+++ Main.hs
@@ -1,33 +1,19 @@
 module Main where
-import LatexNodes (parseDoc,emitDoc)
+import LatexNodes (parseDoc,emitDoc,varDecs)
 import Text.ParserCombinators.Parsec
 import Control.Applicative
 import Text.Printf (printf)
 import System.Environment
 
-stringDoc :: String -> String
-stringDoc s = case parse parseDoc "" s of
-                Left e -> show e
-                Right d -> emitDoc d
-
 doIO :: String -> String -> String -> IO ()
 doIO proLF texF outFile = do
-  p <- readFile proLF
-  t <- stringDoc <$> readFile texF
-  writeFile outFile (printf "%s\n%s\n" p t)
+  p     <- readFile proLF
+  (t,h) <- stringDoc <$> readFile texF
+  writeFile outFile (printf "%s\n%s\n%s\n" h p t)
+  where stringDoc s = case parse parseDoc "" s of
+                        Left e -> error $ "illegal parse:\n" ++ show e
+                        Right d -> (emitDoc d,varDecs d)
 
+-- TODO: sane and helpful command-line arg stuff
 main :: IO ()
 main = do {[p,t,o] <- getArgs; doIO p t o}
-
-{-main = do-}
-  {-[s] <- getArgs-}
-  {-str <- readFile s-}
-  {-putStrLn $ stringDoc str-}
-{-
-main = print $ stringDoc ("TITLE: Test\n"
-                                  ++ "AUTHOR: Yo There\n"
-                                  ++ "\t\t\t- hello\n"
-                                  ++ "\t- ##there##\n"
-                                  ++ "\t- #there#\n"
-                                  ++ "\t\t- sir\n")
--}
